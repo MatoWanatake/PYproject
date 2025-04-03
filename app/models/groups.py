a4e4a5e84b0a92e4a7fd742fe6c69ea5cf4123ef
Michael Brandon <mikebrandon91@gmail.com>
2025-04-02 22:52:05 -0500

    table fixed

--- app/models/groups.py
+++ app/models/groups.py
@@ -1,27 +1,28 @@
-from .db import db, environment,SCHEMA, add_prefix_for_prod
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
 
-class Groups(db.Model):
+class Group(db.Model):
     __tablename__ = 'groups'
 
-    if environment == "production"
-        __table_args__ = { 'schema' : SCHEMA}
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
 
-        id = db.Column(db.Integer, primary_key = True)
-        name = db.Column(db.String(100), nullable = False)
-        description = db.Column(db.Text)
-        created_by = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
-        created_at = db.Column(db.DateTime, default = datetime.utcnow)
-        updated_at = db.Column(db.DateTime, default = datetime.utcnow, onupdate = datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text)
+    created_by = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now(), onupdate=datetime.now())
 
-        members = db.relationship('GroupMember', back_populates= 'group', cascade= "all, delete-orphan")
-        expenses = db.relationship('Expense', backref= 'group', cascade= "all, delete-orphan")
+    members = db.relationship('GroupMember', back_populates='groups', cascade="all, delete-orphan")
+    expenses = db.relationship('Expense', backref='groups', cascade="all, delete-orphan")
 
-        def to_dict(self):
-            return {
-                'Id': self.id,
-                'name': self.name,
-                'description': self.description,
-                'created_by': self.created_by,
-                'create_at': self.created_at,
-                'updated_at': self.updated_at
-            }
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'created_by': self.created_by,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
