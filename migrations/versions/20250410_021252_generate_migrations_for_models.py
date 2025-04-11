3905bc7fafd4ab31a84632c9c728758d2f9d88fd
TechWillie <techwillie5@gmail.com>
2025-04-10 23:00:06 -0400

    more routes configurations

---
+++ migrations/versions/20250410_021252_generate_migrations_for_models.py
@@ -0,0 +1,24 @@
+"""Generate Migrations For Models
+
+Revision ID: b981ef674ed4
+Revises: 
+Create Date: 2025-04-10 02:12:52.581289
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b981ef674ed4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
